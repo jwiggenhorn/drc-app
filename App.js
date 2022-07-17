9b1cbbd8560fb8609896569b08773e8fc3855dcd
jwiggen <jwiggen@siue.edu>
2022-07-17 17:51:07 -0500

    added study key input

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 export default function App() {
+  const [studyKey, setStudyKey] = useState('')
+  const [study, setStudy] = useState()
+
+  async function handleSubmit() {
+    await fetch(`http://localhost:3000/api/studies/key/${studyKey.trim()}`)
+      .then((response) => response.json())
+      .then((data) => setStudy(data))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter a study key:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setStudyKey}
+        value={studyKey}
+      />
+      <Button title="Submit" onPress={handleSubmit} />
       <StatusBar style="auto" />
+      {study && study.name && (
+        <Text>Study key corresponds to study with name: {study.name}</Text>
+      )}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +36,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+})
