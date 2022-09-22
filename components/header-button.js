e25c5c2ae373636e2b274f05926d854cb2649c92
Sam White <samuwhi@siue.edu>
2022-09-22 16:59:21 -0500

    Testing the api call to submit the data.

--- components/header-button.js
+++ components/header-button.js
@@ -2,11 +2,24 @@ import { useContext, useState } from 'react'
 import { View, Button, Modal, Text } from 'react-native'
 import { styles } from '../styles'
 import { TimeContext } from '../App'
+import { API_URL } from '../environment'
 
 export default function HeaderButton() {
   const [isCapturing, setIsCapturing] = useState(false)
   const { setStartTime } = useContext(TimeContext)
   const [modalVisible, setModalVisible] = useState(false)
+
+  async function handleSubmit() {
+    await fetch(`${API_URL}/study/data`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(testData),
+    })
+      .then((data) => console.log(data))
+      .catch((e) => console.error(e))
+  }
 
   return (
     <View style={styles.header}>
@@ -16,6 +29,7 @@ export default function HeaderButton() {
           onPress={() => {
             setIsCapturing(false)
             setModalVisible(true)
+            //TODO: Pause the music
           }}
         />
       ) : (
@@ -45,6 +59,7 @@ export default function HeaderButton() {
                 title="Submit"
                 onPress={() => {
                   //Submit the data
+                  handleSubmit()
                 }}
               />
             </View>
@@ -52,7 +67,8 @@ export default function HeaderButton() {
               <Button
                 title="Cancel"
                 onPress={() => {
-                  //Cancel
+                  setModalVisible(false)
+                  //TODO: resume the music
                 }}
               />
             </View>
