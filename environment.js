28c0d0f93d4893d7e0fb3ca536c71088d813ec1b
jwiggen <jwiggen@siue.edu>
2022-10-22 15:42:26 -0500

    force landscape mode and update production URLs

--- environment.js
+++ environment.js
@@ -1,6 +1,8 @@
-const API_URL = __DEV__ ? 'http://localhost:3000/api' : '[Production API URL]'
+const API_URL = __DEV__
+  ? 'http://localhost:3000/api'
+  : 'https://drc-admin-panel.vercel.app/api'
 const IMAGES_URL = __DEV__
   ? 'http://localhost:3000/images'
-  : '[Production IMAGES URL]'
+  : 'https://drc-admin-panel.vercel.app/images'
 
 export { API_URL, IMAGES_URL }
